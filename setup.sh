b51ed3d1b5dd1d09c4d823356935e73d11aef1f8
tre murillo <treelzebub@users.noreply.github.com>
2022-05-22 09:53:25 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,6 @@
 echo
 echo "Update all the things!"
 echo
-
 sudo apt update && sudo apt upgrade -y
 sudo apt install vim -y
 
@@ -14,25 +13,20 @@ sudo apt install vim -y
 echo
 echo "Installing pi-hole..."
 echo
-
 curl -sSL https://install.pi-hole.net | sudo bash
 
 # unbound
 echo
 echo "Installing unbound..."
 echo
-
 sudo apt install unbound
 wget https://www.internic.net/domain/named.root -qO- | sudo tee /var/lib/unbound/root.hints
 sudo mv pi-hole.conf /etc/unbound/unbound.conf.d/pi-hole.conf
-
 echo
-echo "So far so good. Attempting to init unbound..."
+echo "Attempting to init unbound..."
 echo
-
 sudo service unbound restart
 dig pi-hole.net @127.0.0.1 -p 5335
-
 echo
 echo "Finally, configure Pi-hole to use your recursive DNS server by specifying 127.0.0.1#5335 as the Custom DNS (IPv4)"
-read -p "Press any key to continue..." -n1 -s
+echo
